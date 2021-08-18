348fac362962a479160e30490a944a26b334f0b9
okada-shun <syun19981207@gmail.com>
2021-08-18 22:30:03 +0900

    Fix comment

--- main.go
+++ main.go
@@ -57,17 +57,17 @@ func GetConnection() *gorm.DB {
 
 // User構造体
 type User struct {
-	ID   int
-	Name string
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
 
 // createUser関数で作成されたjwtトークンが入る
-type LoginResponse struct {
+type TokenResponse struct {
 	Token string `json:"token"`
 }
 
 // localhost:8080/user/createでユーザ情報を作成
-// -d {"name":"　"}で名前情報を受け取り、dbに挿入
+// -d {"name":"x"}で名前情報を受け取り、dbに挿入
 // dbのIDからjwtトークンを作成し、トークンを返す
 func createUser(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
@@ -76,32 +76,43 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 		RespondWithError(w, http.StatusBadRequest, "Invalid request")
 		return
 	}
+	// string(body) = {"name": "x"}
 	var user User
 	if err := json.Unmarshal(body, &user); err != nil {
 		RespondWithError(w, http.StatusBadRequest, "JSON Unmarshaling failed .")
 		return
 	}
+	// user = {0 x}
+	// &user = &{0 x}
 	db := GetConnection()
 	defer db.Close()
+	// INSERT INTO `users` (`name`) VALUES ('x')
 	db.Create(&user)
+	// ユーザIDの文字列からjwtトークン作成
 	token, err := CreateToken(strconv.Itoa(user.ID))
 	if err != nil {
 		ErrorResponse(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-	Success(w, &LoginResponse{
+	// token = "生成されたトークンの文字列"
+	Success(w, &TokenResponse{
 		Token: token,
 	})
+	// {"token":"生成されたトークンの文字列"}が返る
 }
 
 // ユーザIDからjwtトークンを作成
-// 期限は1時間
+// 有効期限は1時間に設定
+// jwtのペイロードにはユーザIDと有効期限の時刻を設定
 func CreateToken(userID string) (string, error) {
+	// HS256は256ビットのハッシュ値を生成するアルゴリズム
 	token := jwt.New(jwt.GetSigningMethod("HS256"))
+	// ペイロードにユーザIDと有効期限の時刻(1時間後)を設定
 	token.Claims = jwt.MapClaims{
 		"userId": userID,
 		"exp":    time.Now().Add(time.Hour * 1).Unix(),
 	}
+	// 秘密鍵で署名
 	var secretKey = "secret"
 	tokenString, err := token.SignedString([]byte(secretKey))
 	if err != nil {
@@ -141,28 +152,41 @@ type UserResponse struct {
 	Name string `json:"name"`
 }
 
-// -H "x-token: "でトークン情報を受け取り、認証
+// -H "x-token:yyy"でトークン情報を受け取り、認証
 // トークンからユーザIDを取り出し、dbからそのIDのユーザの名前情報を取り出し、返す
 func getUser(w http.ResponseWriter, r *http.Request) {
 	tokenString := r.Header.Get("x-token")
+	// tokenString = yyy
+	// VerifyToken関数で認証
 	token, err := VerifyToken(tokenString)
 	if err != nil {
 		ErrorResponse(w, http.StatusBadRequest, err.Error())
 		return
 	}
+	/*例
+	token = &{eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MjkyOTIzNjAsInVzZXJJZCI6IjkifQ.
+	QNASvnRbfNTwML9pKjVkA3EusbfxLsQr711iqA2hJLo 0xc00000e0c0
+	map[alg:HS256 typ:JWT] map[exp:1.62929236e+09 userId:9] QNASvnRbfNTwML9pKjVkA3EusbfxLsQr711iqA2hJLo true}
+	*/
 	db := GetConnection()
 	defer db.Close()
 	claims := token.Claims.(jwt.MapClaims)
+	// claims = map[exp:1.62929236e+09 userId:9]
 	id := claims["userId"]
+	// id = 9
 	var user User
+	// SELECT * FROM `users`  WHERE (id = '9')
 	db.Where("id = ?", id).Find(&user)
 	Success(w, &UserResponse{
 		Name: user.Name,
 	})
+	// {"name":"x"}が返る
+	// 有効期限が切れると{"code":400,"message":"Token is expired"}が返る
 }
 
 // jwtトークンを認証する
 func VerifyToken(tokenString string) (*jwt.Token, error) {
+	// "secret"は秘密鍵
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		return []byte("secret"), nil
 	})
@@ -172,22 +196,30 @@ func VerifyToken(tokenString string) (*jwt.Token, error) {
 	return token, nil
 }
 
-// -H "x-token: "でトークン情報を受け取り、認証
-// -d {"name":" "}で更新する名前情報を受け取る
+// -H "x-token:yyy"でトークン情報を受け取り、認証
+// -d {"name":"z"}で更新する名前情報を受け取る
 // トークンからユーザIDを取り出し、dbからそのIDのユーザの名前情報を更新
 func updateUser(w http.ResponseWriter, r *http.Request) {
 	tokenString := r.Header.Get("x-token")
+	// tokenString = yyy
+	// VerifyToken関数で認証
 	token, err := VerifyToken(tokenString)
 	if err != nil {
 		ErrorResponse(w, http.StatusBadRequest, err.Error())
 		return
 	}
+	/*例
+	token = &{eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MjkyOTIzNjAsInVzZXJJZCI6IjkifQ.
+	QNASvnRbfNTwML9pKjVkA3EusbfxLsQr711iqA2hJLo 0xc00000e0c0
+	map[alg:HS256 typ:JWT] map[exp:1.62929236e+09 userId:9] QNASvnRbfNTwML9pKjVkA3EusbfxLsQr711iqA2hJLo true}
+	*/
 	body, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
 	if err != nil {
 		RespondWithError(w, http.StatusBadRequest, "Invalid request")
 		return
 	}
+	// string(body) = {"name": "z"}
 	var user User
 	if err := json.Unmarshal(body, &user); err != nil {
 		RespondWithError(w, http.StatusBadRequest, "JSON Unmarshaling failed .")
@@ -196,6 +228,10 @@ func updateUser(w http.ResponseWriter, r *http.Request) {
 	db := GetConnection()
 	defer db.Close()
 	claims := token.Claims.(jwt.MapClaims)
+	// claims = map[exp:1.62929236e+09 userId:9]
 	id := claims["userId"]
+	// id = 9
+	// dbでnameを更新
+	// UPDATE `users` SET `name` = 'z'  WHERE (id = '9')
 	db.Model(&user).Where("id = ?", id).Update("name", user.Name)
 }
