c395d568949a2ed8c90b5b56947873b8e5fa6b7c
okada-shun <syun19981207@gmail.com>
2021-09-02 18:35:09 +0900

    Remove id from tables

--- mysql/ddl.sql
+++ mysql/ddl.sql
@@ -4,15 +4,13 @@ USE `game_user`;
 
 DROP TABLE IF EXISTS `game_user`.`users`;
 CREATE TABLE IF NOT EXISTS `game_user`.`users`(
-  `id` INT PRIMARY KEY AUTO_INCREMENT,
-  `user_id` VARCHAR(100) NOT NULL,
+  `user_id` CHAR(36) PRIMARY KEY NOT NULL,
   `name` VARCHAR(32) NOT NULL
 );
 
 DROP TABLE IF EXISTS `game_user`.`characters`;
 CREATE TABLE IF NOT EXISTS `game_user`.`characters`(
-  `id` INT PRIMARY KEY AUTO_INCREMENT,
-  `character_id` VARCHAR(100) NOT NULL,
+  `character_id` CHAR(36) PRIMARY KEY NOT NULL,
   `name` VARCHAR(32) NOT NULL,
   `weight` INT NOT NULL
 );
@@ -30,8 +28,7 @@ INSERT INTO characters(character_id, name, weight) VALUES (UUID(), 'Carol_N', 60
 
 DROP TABLE IF EXISTS `game_user`.`user_characters`;
 CREATE TABLE IF NOT EXISTS `game_user`.`user_characters`(
-  `id` INT PRIMARY KEY AUTO_INCREMENT,
-  `user_character_id` VARCHAR(100) NOT NULL,
-  `user_id` VARCHAR(100) NOT NULL,
-  `character_id` VARCHAR(100) NOT NULL
+  `user_character_id` CHAR(36) PRIMARY KEY NOT NULL,
+  `user_id` VARCHAR(36) NOT NULL,
+  `character_id` VARCHAR(36) NOT NULL
 );
